752b2cfd97800d958578d39a998a698588e69c6d
Prechh <adamadrien21@gmail.com>
2025-05-20 15:10:03 +0200

    ajustement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: [
+        "**/node_modules",
+        "**/.git",
+        "**/C:/Users/Adrien/Application Data/**", // <- ✅ chemin corrigé
+        "**/C:/Users/Adrien/**", // ← ajoute cette ligne !
+      ],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
