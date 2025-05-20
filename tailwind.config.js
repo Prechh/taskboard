594d26c7ed87f0accdb99af8c2fee6c57083e85f
Prechh <adamadrien21@gmail.com>
2025-05-20 16:02:39 +0200

    Fix chemin Tailwind pour le build Render

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // pour le dossier app/
+    "./pages/**/*.{js,ts,jsx,tsx}", // si jamais tu as encore des pages legacy
+    "./components/**/*.{js,ts,jsx,tsx}", // si tu as un dossier components/
+    "./src/**/*.{js,ts,jsx,tsx}", // tout le src
+  ],
   theme: {
     extend: {},
   },
